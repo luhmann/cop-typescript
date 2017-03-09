1ea68286f021388ea8f3a4249ae9896d8e30f750
Jan Florian Dietrich <jan-florian.dietrich@epost-dev.de>
2017-03-09 13:03:37 +0100

    Tweaks

--- app/Button.tsx
+++ app/Button.tsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components'
 
 const Button = styled.button`
-  border-radius: 4px
-  color: #fff
+  border: none;
+  border-radius: 4px;
+  box-shadow: none;
+  color: #fff;
 `
 
 export default Button
